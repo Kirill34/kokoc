717706393176c9f01a924399d3b3f15c5eb69f21
KKS <kirill.kuliuckin2016@yandex.ru>
2023-09-30 07:35:46 +0300

    all departaments view

--- src/main/java/controller/DepartamentController.java
+++ src/main/java/controller/DepartamentController.java
@@ -1,10 +1,15 @@
 package controller;
 
 import model.Departament;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import repo.DepartamentRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/departament")
@@ -19,5 +24,17 @@ public class DepartamentController {
         Departament departament = new Departament(name);
         departamentRepository.save(departament);
         return departament.getId();
+    }
+
+    @GetMapping("/all")
+    public List<Departament> getAll()
+    {
+        return departamentRepository.findAllByIdGreaterThan(0L);
+    }
+
+    @GetMapping("/view")
+    public ModelAndView getPageForAllSports(Model model)
+    {
+        return new ModelAndView("allDepartamentsView");
     }
 }
--- src/main/java/repo/DepartamentRepository.java
+++ src/main/java/repo/DepartamentRepository.java
@@ -3,5 +3,8 @@ package repo;
 import model.Departament;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DepartamentRepository extends CrudRepository<Departament,Long> {
+    public List<Departament> findAllByIdGreaterThan(Long id);
 }
